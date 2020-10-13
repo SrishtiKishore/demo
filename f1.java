0b2c649c0272b2e6523ef1e3548724710926edda
SrishtiKishore <srishtikishore26@gmail.com>
2020-10-13 12:27:07 +0530

    commit to modified f1

--- f1.java
+++ f1.java
@@ -2,7 +2,9 @@
 public class f1{
 
 public static void main(String args[]){
-System.out.print("this is file 1");
+System.out.println("this is file 1");
+System.out.println("new addition");
+System.out.println("new removal");
 
 }
 
